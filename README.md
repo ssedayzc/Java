a9248d85fa57c80112e73a26bb581870fddaf17b
ssedayzc <seda.yazici08@erzurum.edu.tr>
2023-01-29 23:54:02 +0300

    README.md updated

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# Harmonik Seri
-Java ile girilen sayının harmonik serisini bulan program yazacağız.
+# Yıldızlar ile desen oluşturma
 
-Harmonik Seri Formülü :
-
-1 + (1/2) + (1/3)+ (1/4) + ... + (1/n)
+for döngüsü kullanarak yıldızlar ile baklava dilimi yazdıran program
---
+++ src/YildizlarlaElmas.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class YildizlarlaElmas {
+    public static void main(String[] args){
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Bir Sayı Giriniz :");
+        int n = scan.nextInt();
+
+        for (int i = 0; i <= n ; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+        for (int i = n-1; i >= 0  ; i--) {
+
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+
+
+    }
+}
